59f8c2d4d13581b385226c7a8fc75de948dcd91d
theeddiem <eddieknaz@gmail.com>
2020-09-19 03:23:27 +0300

    Initial commit

---
+++ src/main/java/com/eddieknaz/springboot/fastsurvey/FastsurveyApplication.java
@@ -0,0 +1,13 @@
+package com.eddieknaz.springboot.fastsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastsurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastsurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eddieknaz/springboot/fastsurvey/FastsurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eddieknaz.springboot.fastsurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastsurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
