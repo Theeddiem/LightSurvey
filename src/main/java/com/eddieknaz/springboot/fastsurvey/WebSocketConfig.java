73e6a84244c1168ffb93a9a6df142887a2d355d9
theeddiem <eddieknaz@gmail.com>
2020-10-21 20:21:00 +0300

    STOMP REATLTIME

--- src/main/java/com/eddieknaz/springboot/fastsurvey/WebSocketConfig.java
+++ src/main/java/com/eddieknaz/springboot/fastsurvey/WebSocketConfig.java
@@ -13,11 +13,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
+        config.setPreservePublishOrder(true);
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/api/survey").setAllowedOrigins("*").withSockJS();
+
     }
+
 
 }
