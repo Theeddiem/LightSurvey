42eb55fbd66b965c51575c96d61363211958ba69
theeddiem <eddieknaz@gmail.com>
2022-02-06 21:04:58 +0200

    make it better

---
+++ src/main/resources/static/precache-manifest.8ba1e898eb8b41c3509559189218ad25.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d49af76acb9c2142f7a",
+    "url": "/css/app.4c4e57f0.css"
+  },
+  {
+    "revision": "19d9a2c86a46f85549ee",
+    "url": "/css/chunk-vendors.5c3e7589.css"
+  },
+  {
+    "revision": "5ee0f2d1a1e6e14d8c4d8fa7f18c3326",
+    "url": "/fonts/luckiestguy.5ee0f2d1.ttf"
+  },
+  {
+    "revision": "1f683b2d323499417d138657e50c3a06",
+    "url": "/img/copyLink_icon.1f683b2d.svg"
+  },
+  {
+    "revision": "c4f927fa22aae43bd6630c0388be19cb",
+    "url": "/img/install_icon.c4f927fa.svg"
+  },
+  {
+    "revision": "cb5206400362d544e1ff139b7cedb4cc",
+    "url": "/img/pie-chart_icon.cb520640.svg"
+  },
+  {
+    "revision": "060425cabf7163ccc29032fe5fc02500",
+    "url": "/img/voters_icon.060425ca.svg"
+  },
+  {
+    "revision": "907f7cd9d70387a7580f50ca7808ea4d",
+    "url": "/img/whatsApp_icon.907f7cd9.svg"
+  },
+  {
+    "revision": "b53021ccd806134db60b474ae9f3aa90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d49af76acb9c2142f7a",
+    "url": "/js/app.6b01d15a.js"
+  },
+  {
+    "revision": "19d9a2c86a46f85549ee",
+    "url": "/js/chunk-vendors.7542ff88.js"
+  },
+  {
+    "revision": "cb5cb546be170e9093c99c3de7e5cff3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
